e621e7b91be64f3c6761197017a2be8e06a90e68
leo60228 <leo@60228.dev>
2021-03-02 12:56:07 -0500

    journald

--- idol_bot/src/logger.rs
+++ idol_bot/src/logger.rs
@@ -1,23 +1,36 @@
 use anyhow::Result;
 use chrono::prelude::*;
+use libsystemd::logging::{journal_send, Priority};
 use log::*;
+use std::collections::HashMap;
+use std::sync::atomic::{AtomicBool, Ordering};
 
 pub fn init() -> Result<()> {
-    let syslog_fmt = syslog::Formatter3164 {
-        facility: syslog::Facility::LOG_USER,
-        hostname: None,
-        process: "idol_bot".to_owned(),
-        pid: std::process::id() as _,
-    };
-    let (syslog, syslog_err): (fern::Dispatch, _) = match syslog::unix(syslog_fmt) {
-        Ok(syslog) => (
-            fern::Dispatch::new()
-                .level(log::LevelFilter::Debug)
-                .chain(syslog),
-            None,
-        ),
-        Err(err) => (fern::Dispatch::new(), Some(err)),
-    };
+    let journald_works = AtomicBool::new(true);
+    let syslog = fern::Output::call(move |record| {
+        if journald_works.load(Ordering::SeqCst) {
+            let priority = match record.level() {
+                Level::Error => Priority::Error,
+                Level::Warn => Priority::Warning,
+                Level::Info => Priority::Notice,
+                Level::Debug => Priority::Info,
+                Level::Trace => Priority::Debug,
+            };
+            let mut fields = HashMap::new();
+            fields.insert("SYSLOG_IDENTIFIER", "idol_bot".to_string());
+            if let Some(file) = record.file().or_else(|| record.module_path()) {
+                fields.insert("CODE_FILE", file.to_string());
+            }
+            if let Some(line) = record.line() {
+                fields.insert("CODE_LINE", line.to_string());
+            }
+            if let Err(err) = journal_send(priority, &record.args().to_string(), fields.into_iter())
+            {
+                journald_works.store(false, Ordering::SeqCst);
+                warn!("journald error: {}", err);
+            }
+        }
+    });
     fern::Dispatch::new()
         .level(log::LevelFilter::Warn)
         .level_for("idol_bot", log::LevelFilter::Trace)
@@ -37,10 +50,17 @@ pub fn init() -> Result<()> {
                 })
                 .chain(std::io::stdout()),
         )
-        .chain(syslog)
+        .chain(
+            fern::Dispatch::new()
+                .format(move |out, message, record| {
+                    out.finish(format_args!(
+                        "[{}] {}",
+                        record.module_path().unwrap_or("<unknown>"),
+                        message
+                    ))
+                })
+                .chain(syslog),
+        )
         .apply()?;
-    if let Some(err) = syslog_err {
-        warn!("Error setting up syslog: {}", err);
-    }
     Ok(())
 }
